4d7fba2362e7244002903ecd6a401ea5751e5ffd
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-05-21 01:58:20 +0800

    refactor

--- main.php
+++ main.php
@@ -12,229 +12,224 @@ class Mybot extends EventHandler
 {
     protected $adminId = '905361440';
     public const STORAGE = './assets';
+    // dont use yield outside any event handler method
+    public function urban($query)
+    {
+        $data = RapidApi::urban($query);
+        $data = $data->list[0];
+        $text = <<<MD
+**definition:** *{$data->definition}*
+**permalink:** `{$data->permalink}`
+MD;
+        $this->messages->sendMessage([
+            'peer' => $this->peer,
+            'reply_to_msg_id' => $this->chatId,
+            'parse_mode' => 'Markdown',
+            'message' => $text,
+        ]);
+    }
+    public function simpleImage($text)
+    {
+        $images = new Gambar($text, 'dark');
+        $text = $images->getResult(
+            $this::STORAGE . '/result.jpg',
+            'image/jpeg',
+            100
+        );
+        $this->messages->sendMedia([
+            'peer' => $this->peer,
+            'reply_to_msg_id' => $this->chatId,
+            'media' => [
+                '_' => 'inputMediaUploadedPhoto',
+                'file' => $this::STORAGE . '/result.jpg',
+            ],
+            'message' => $text,
+        ]);
+    }
+    public function fortune()
+    {
+        $text = Fortune::make();
+        $this->messages->sendMessage([
+            'peer' => $this->peer,
+            'reply_to_msg_id' => $this->chatId,
+            'message' => '`' . $text . '`',
+            'parse_mode' => 'Markdown',
+        ]);
+    }
+    public function animate($text)
+    {
+        $temposleep = 300000;
+        $oldtext = '';
+        $num = 1;
+        $this->messages->editMessage([
+            'no_webpage' => true,
+            'peer' => $this->peer,
+            'id' => $this->chatId,
+            'message' => "|",
+            'entities' => [
+                [
+                    '_' => 'messageEntityCode',
+                    'offset' => 0,
+                    'length' => 1,
+                ],
+            ],
+        ]);
+        usleep($temposleep);
+        while ($oldtext != $text) {
+            $oldtext = mb_substr($text, 0, $num);
+            $oldtext1 = $oldtext . "|";
+            $this->messages->editMessage([
+                'no_webpage' => true,
+                'peer' => $this->peer,
+                'id' => $this->chatId,
+                'message' => $oldtext1,
+                'parse_mode' => 'HTML',
+                'entities' => [
+                    [
+                        '_' => 'messageEntityCode',
+                        'offset' => 0,
+                        'length' => strlen($oldtext1),
+                    ],
+                ],
+            ]);
+            usleep($temposleep);
+            $this->messages->editMessage([
+                'no_webpage' => true,
+                'peer' => $this->peer,
+                'id' => $this->chatId,
+                'message' => $oldtext,
+                'parse_mode' => 'HTML',
+                'entities' => [
+                    [
+                        '_' => 'messageEntityCode',
+                        'offset' => 0,
+                        'length' => strlen($oldtext),
+                    ],
+                ],
+            ]);
+            $num++;
+            usleep($temposleep);
+        }
+    }
+    public function loopCommand($text, $count)
+    {
+        for ($x = 0; $x < $count; $x++) {
+            if ($text === false) {
+                $buff = $text = Fortune::make();
+            }
 
-    public function onAny($update)
+            $this->messages->sendMessage([
+                'peer' => $this->peer,
+                'message' => $text,
+            ]);
+
+            if (isset($buff) && $buff == $text) {
+                $text = false;
+            }
+        }
+    }
+    public function upfile($file)
+    {
+        $this->messages->sendMedia([
+            'peer' => $this->peer,
+            'reply_to_msg_id' => $this->chatId,
+            'parse_mode' => 'Markdown',
+            'media' => [
+                '_' => 'inputMediaUploadedDocument',
+                'file' => $file,
+            ],
+            'message' => '*' . basename($file) . '* has been uploaded',
+        ]);
+    }
+    public function evalCommand($text)
+    {
+        ob_start();
+        eval($text);
+        $text = ob_get_clean();
+        $this->messages->editMessage([
+            'peer' => $this->peer,
+            'message' => $text,
+            'id' => $this->chatId,
+        ]);
+    }
+    // you can use yield inside this method
+    public function onAny($update) // on any event
     {
         if (empty($update['message']['message'])) {
             return;
         }
-        $message = $update['message']['message'];
-        $chat_id = $update['message']['id'];
-        $from_id = @$update['message']['from_id'];
-        $peer = $update;
+        $this->message = $update['message']['message'];
+        $this->chatId = $update['message']['id'];
+        $this->fromId = @$update['message']['from_id'];
+        $this->peer = $update;
 
-        if (preg_match('/^\/urban/i', $message)) {
-            if (preg_match('/\s"(.+)"/i', $message, $match)) {
-                $query = $match[1];
-            }
+        if (preg_match('/^\/urban/i', $this->message)) {
             try {
-                $data = (yield RapidApi::urban($query));
-                $data = $data->list[0];
-                $text = <<<MD
-**definition:** *{$data->definition}*
-**permalink:** `{$data->permalink}`
-MD;
-                yield $this->messages->sendMessage([
-                    'peer' => $peer,
-                    'reply_to_msg_id' => $chat_id,
-                    'parse_mode' => 'Markdown',
-                    'message' => $text,
-                ]);
+                if (preg_match('/\s"(.+)"/i', $this->message, $match)) {
+                    $query = $match[1];
+                }
+                yield $this->urban($query);
             } catch (\danog\MadelineProto\Exception | \Exception $e) {
                 yield $this->messages->sendMessage([
-                    'peer' => $peer,
-                    'reply_to_msg_id' => $chat_id,
+                    'peer' => $this->peer,
+                    'reply_to_msg_id' => $this->chatId,
                     'message' => $e->getMessage(),
                 ]);
             }
-        } elseif (preg_match('/^\/simpleimage/i', $message)) {
-            preg_match('/\s"(.+)"/i', $message, $match)
+        } elseif (preg_match('/^\/simpleimage/i', $this->message)) {
+            preg_match('/\s"(.+)"/i', $this->message, $match)
                 ? ($text = $match[1])
                 : ($text = Fortune::make());
-
-            yield ($simpleimage = new Gambar($text, 'dark'));
-            $text = (yield $simpleimage->getResult(
-                $this::STORAGE . '/result.jpg',
-                'image/jpeg',
-                100
-            ));
-            yield $this->messages->sendMedia([
-                'peer' => $peer,
-                'reply_to_msg_id' => $chat_id,
-                'media' => [
-                    '_' => 'inputMediaUploadedPhoto',
-                    'file' => $this::STORAGE . '/result.jpg',
-                ],
-                'message' => $text,
-            ]);
-        } elseif (preg_match('/^\/fortune/i', $message)) {
-            $text = Fortune::make();
-            yield $this->messages->sendMessage([
-                'peer' => $peer,
-                'reply_to_msg_id' => $chat_id,
-                'message' => '`' . $text . '`',
-                'parse_mode' => 'Markdown',
-            ]);
-        } elseif (@$from_id == $this->adminId) {
+            yield $this->simpleImage($text);
+        } elseif (preg_match('/^\/fortune/i', $this->message)) {
+            yield $this->fortune();
+        } elseif (@$this->fromId == $this->adminId) {
             // admin commmand
-            if (preg_match('/^\/animate/', $message)) {
-                preg_match('/\s"(.+)"/i', $message, $match)
+            if (preg_match('/^\/animate/', $this->message)) {
+                preg_match('/\s"(.+)"/i', $this->message, $match)
                     ? ($text = $match[1])
                     : ($text = Fortune::make());
-
-                $temposleep = 300000;
-                $oldtext = '';
-                $num = 1;
-                $this->messages->editMessage([
-                    'no_webpage' => true,
-                    'peer' => $peer,
-                    'id' => $chat_id,
-                    'message' => "|",
-                    'entities' => [
-                        [
-                            '_' => 'messageEntityCode',
-                            'offset' => 0,
-                            'length' => 1,
-                        ],
-                    ],
-                ]);
-                usleep($temposleep);
-                while ($oldtext != $text) {
-                    $oldtext = mb_substr($text, 0, $num);
-                    $oldtext1 = $oldtext . "|";
-                    $this->messages->editMessage([
-                        'no_webpage' => true,
-                        'peer' => $peer,
-                        'id' => $chat_id,
-                        'message' => $oldtext1,
-                        'parse_mode' => 'HTML',
-                        'entities' => [
-                            [
-                                '_' => 'messageEntityCode',
-                                'offset' => 0,
-                                'length' => strlen($oldtext1),
-                            ],
-                        ],
-                    ]);
-                    usleep($temposleep);
-                    $this->messages->editMessage([
-                        'no_webpage' => true,
-                        'peer' => $peer,
-                        'id' => $chat_id,
-                        'message' => $oldtext,
-                        'parse_mode' => 'HTML',
-                        'entities' => [
-                            [
-                                '_' => 'messageEntityCode',
-                                'offset' => 0,
-                                'length' => strlen($oldtext),
-                            ],
-                        ],
-                    ]);
-                    $num++;
-                    usleep($temposleep);
-                }
-            } elseif (preg_match('/^\/loop/i', $message)) {
+                yield $this->animate($text);
+            } elseif (preg_match('/^\/loop/i', $this->message)) {
                 try {
-                    preg_match('/"\s(.*\d)/', $message, $match)
+                    preg_match('/"\s(.*\d)/', $this->message, $match)
                         ? ($count = $match[1])
                         : ($count = 1);
-
-                    preg_match('/^\/loop\s"(.*)"/i', $message, $match)
+                    preg_match('/^\/loop\s"(.*)"/i', $this->message, $match)
                         ? ($text = $match[1])
                         : ($text = false);
-
-                    for ($x = 0; $x < $count; $x++) {
-                        if ($text === false) {
-                            $buff = $text = Fortune::make();
-                        }
-
-                        yield $this->messages->sendMessage([
-                            'peer' => $peer,
-                            'message' => $text,
-                        ]);
-
-                        if (isset($buff) && $buff == $text) {
-                            $text = false;
-                        }
-                    }
+                    yield $this->loopCommand($text, $count);
                 } catch (\danog\MadelineProto\Exception | Exception $e) {
                     yield $this->messages->editMessage([
-                        'peer' => $peer,
-                        'id' => $chat_id,
+                        'peer' => $this->peer,
+                        'id' => $this->chatId,
                         'message' => $e->getMessage(),
                     ]);
                 }
-            } elseif (preg_match('/^\/debug/i', $message)) {
-                yield $this->messages->editMessage([
-                    'no_webpage' => true,
-                    'peer' => $peer,
-                    'id' => $chat_id,
-                    'message' => json_encode(
-                        $update,
-                        JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES
-                    ),
-                ]);
-            } elseif (preg_match('/^\/upfile/i', $message)) {
-                preg_match('/\s"(.+)"/', $message, $match)
-                    ? ($file = $match[1])
-                    : ($file = $this::STORAGE . '/default.jpg');
-
+            } elseif (preg_match('/^\/upfile/i', $this->message)) {
                 try {
-                    yield $this->messages->sendMedia([
-                        'peer' => $peer,
-                        'reply_to_msg_id' => $chat_id,
-                        'parse_mode' => 'Markdown',
-                        'media' => [
-                            '_' => 'inputMediaUploadedDocument',
-                            'file' => $file,
-                        ],
-                        'message' =>
-                            '*' . basename($file) . '* has been uploaded',
-                    ]);
+                    preg_match('/\s"(.+)"/', $this->message, $match)
+                        ? ($file = $match[1])
+                        : ($file = $this::STORAGE . '/default.jpg');
+
+                    yield $this->upfile($file);
                 } catch (\danog\MadelineProto\Exception | Exception $e) {
                     yield $this->messages->editMessage([
-                        'peer' => $peer,
-                        'id' => $chat_id,
+                        'peer' => $this->peer,
+                        'id' => $this->chatId,
                         'message' => $e->getMessage(),
                     ]);
                 }
-            } elseif (preg_match('/^\/eval/i', $message)) {
+            } elseif (preg_match('/^\/eval/i', $this->message)) {
                 try {
-                    ob_start();
-                    eval(substr($message, 6));
-                    $text = ob_get_clean();
+                    $text = substr($this->message, 6);
+                    yield $this->evalCommand($text);
                 } catch (\danog\MadelineProto\Exception | \ParseError $e) {
                     $text = $e->getMessage();
-                    $this->logger($text);
-                }
-                yield $this->messages->editMessage([
-                    'peer' => $peer,
-                    'message' => $text,
-                    'id' => $chat_id,
-                ]);
-            } elseif (preg_match('/^\/invite/i', $message)) {
-                try {
-                    $dialogs = (yield $this->getDialogs());
-                    foreach ($dialogs as $dialog) {
-                        if (isset($dialog['user_id'])) {
-                            $dialogId[] = $dialog['user_id'];
-                        }
-                    }
-
-                    preg_match('/\s"(.+)"/i', $message, $match)
-                        ? ($users = [$match[1]])
-                        : ($users = $dialogId);
-
-                    yield $this->channels->inviteToChannel([
-                        'channel' => '@BHSecFortune',
-                        'users' => $users,
-                    ]);
-                } catch (\danog\MadelineProto\Exception | \Exception $e) {
                     yield $this->messages->editMessage([
-                        'peer' => $peer,
-                        'id' => $chat_id,
-                        'message' => $e->getMessage(),
+                        'peer' => $this->peer,
+                        'id' => $this->chatId,
+                        'message' => $text,
                     ]);
                 }
             }
@@ -246,7 +241,7 @@ MD;
 $settings = [
     'logger' => [
         'param' => MyBot::STORAGE . '/Madeline.log',
-        'logger_level' => 8,
+        'logger_level' => 5,
     ],
     'max_tries' => [
         'query' => 1,
