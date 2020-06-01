4ee83d51c273db857f9d635afd21838bbbdec3b1
Cvar1984 <gedzsarjuncomuniti@gmail.com>
2020-06-02 00:43:55 +0800

    liteotp update

--- src/Madeline/Command/LiteOtp.php
+++ src/Madeline/Command/LiteOtp.php
@@ -17,12 +17,13 @@ trait LiteOtp
             \Cvar1984\LiteOtp\Otp::jdid($numberPhone);
             \Cvar1984\LiteOtp\Otp::tokopedia($numberPhone);
             \Cvar1984\LiteOtp\Otp::phd($numberPhone);
+            \Cvar1984\LiteOtp\Otp::pedulisehat($numberPhone);
         endfor;
 
         yield $this->messages->sendMessage([
             'peer' => $peer,
             'reply_to_msg_id' => $chatId,
-            'message' => 'Done sending 3 otp 3 times'
+            'message' => 'Done sending otp 3 times'
         ]);
     }
 }
